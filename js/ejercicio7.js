b69517e1fe4b3eb4c08dfb9edeef8cb5ac698913
Gaston <gastoncorrea90e@gmail.com>
2020-12-09 18:37:48 -0300

    Se comenzó con el ejercicio 7, se creo variables

---
+++ js/ejercicio7.js
@@ -0,0 +1,5 @@
+//escriba un programa que pida 3 numeros y diga cual es el mayor de los 3.
+
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+let num3 = parseInt(prompt("Ingrese el tercer número"));
