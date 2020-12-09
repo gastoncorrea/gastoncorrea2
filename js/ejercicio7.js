cd8215de92c39d6bb566eec66219eb2245a0faeb
Gaston <gastoncorrea90e@gmail.com>
2020-12-09 18:57:50 -0300

    Se hizo la primera toma de decisión y se cambio el script del index para que funcione el ejercicio7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -3,3 +3,11 @@
 let num1 = parseInt(prompt("Ingrese el primer número"));
 let num2 = parseInt(prompt("Ingrese el segundo número"));
 let num3 = parseInt(prompt("Ingrese el tercer número"));
+
+if(num1 > num2 && num1 > num3){
+    document.write("El primer número es el mayor: " + num1);
+}else if(num1 == num2 && num1 > num3){
+    document.write("El primer y segundo número son los mayores: " + num1); 
+}else if(num1 > num2 && num1 == num3){
+    document.write("El primer y tercer número son los mayores: " + num1);
+}
