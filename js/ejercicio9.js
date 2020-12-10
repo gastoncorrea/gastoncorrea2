6fd7e52099ccaa2cc51870b7ad17ff4d7746ea01
Gaston <gastoncorrea90e@gmail.com>
2020-12-09 21:08:44 -0300

    Se agrego codigo para poder analizar en el ejercicio 9 frases con mayusculas y minusculas

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -7,8 +7,8 @@ document.write("<br> La frase tiene " + cadena1.length + " caracteres <br>");
 document.write("Vocales encontradas: ");
 
 for(let i = 0; i < cadena1.length; i++){
-    if(cadena1.charAt(i) == "a" || cadena1.charAt(i) == "e" || cadena1.charAt(i) == "i"
-    || cadena1.charAt(i) == "o" || cadena1.charAt(i) == "u"){
+    if(cadena1.charAt(i).toLowerCase() == "a" || cadena1.charAt(i).toLowerCase() == "e" || cadena1.charAt(i).toLowerCase() == "i"
+    || cadena1.charAt(i).toLowerCase() == "o" || cadena1.charAt(i).toLowerCase() == "u"){
 
         document.write(cadena1.charAt(i));
         contador++;
